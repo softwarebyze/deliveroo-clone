196680ee680aaf4515cbf970c8e31aadd6013ac4
softwarebyze <zackebenfeld@gmail.com>
2023-06-29 14:24:30 -0400

    image size

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -17,7 +17,7 @@ const RestaurantCard = ({
 }) => {
   return (
     <TouchableOpacity className="bg-white mr-3 shadow">
-      <Image source={{ uri: imageUrl }} className="h-64 w-64 rounded-sm" />
+      <Image source={{ uri: imageUrl }} className="h-36 w-64 rounded-sm" />
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2">{title}</Text>
         <View className="flex-row items-center space-x-1">
